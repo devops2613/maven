32ae04af73d4198aa8bc91ea7274bd8025c58fd0
devops2613 <msdevops2613@gmail.com>
2026-03-11 05:46:51 +0530

    uncommented few lines

--- src/main/java/calciPkg/myCalci.java
+++ src/main/java/calciPkg/myCalci.java
@@ -13,9 +13,9 @@ public class myCalci {
         return a * b;
     }
 
-//    int div(int a, int b) {
-//        return a * b;
-//    }
+    int div(int a, int b) {
+        return a * b;
+    }
 
 
     public static void main(String[] args) {
@@ -23,6 +23,6 @@ public class myCalci {
         System.out.println("Sum is : " + calci.sum(20, 10));
         System.out.println("Diff is : " + calci.diff(20, 10));
         System.out.println("Mul is : " + calci.mul(20, 10));
-//        System.out.println("Div is : " + calci.mul(20, 10));
+        System.out.println("Div is : " + calci.mul(20, 10));
     }
 }
